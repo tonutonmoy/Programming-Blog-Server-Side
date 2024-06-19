08379b936d5544428b46e97647221f6b7fbf8fa5
tonutonmoy <www.tonutonmoy12@gmail.com>
2024-06-19 13:44:04 +0600

    trying

--- dist/resolvers/Mutation/Mutation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Mutation = void 0;
-const auth_1 = require("./auth");
-const post_1 = require("./post");
-const profile_1 = require("./profile");
-const user_1 = require("./user");
-exports.Mutation = Object.assign(Object.assign(Object.assign(Object.assign({}, auth_1.authResolvers), post_1.postResolvers), profile_1.profileResolvers), user_1.userResolvers);
