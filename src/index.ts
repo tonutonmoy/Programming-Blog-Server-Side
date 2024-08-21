53f221f881b6c5e1786a3ba133ada3ffd8b12a34
tonmoy <www.tonutonmoy12@gmail.com>
2024-08-21 12:00:02 +0600

    done

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,8 @@
-// import { ApolloServer } from "@apollo/server";
-// import { startStandaloneServer } from "@apollo/server/standalone";
-
-// import { PrismaClient, Prisma } from "@prisma/client";
-// import { DefaultArgs } from "@prisma/client/runtime/library";
-// import { jwtHelper } from "./utils/jwtHelper";
-// import { typeDefs } from "./schema";
-// import { resolvers } from "./resolvers";
-
-// export const prisma = new PrismaClient();
-
-// interface Context {
-//   prisma: any;
-//   userInfo: any | null;
-// }
-
-// const main = async () => {
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//   });
-
-//   const { url } = await startStandaloneServer(server, {
-//     listen: { port: 4000 },
-//     context: async ({ req }): Promise<Context | undefined> => {
-//       try {
-//         const userInfo = await jwtHelper.getUserInfoFromToken(
-//           req.headers.authorization as string
-//         );
-//         return {
-//           prisma,
-//           userInfo,
-//         };
-//       } catch (error) {
-//         console.error("Error in context function:", error);
-//         return {
-//           prisma,
-//           userInfo: null,
-//         };
-//       }
-//     },
-//   });
-
-//   console.log(`Server ready at: ${url}`);
-// };
-
-// main().catch((error) => {
-//   console.error("Error starting server:", error);
-// });
 
 
 
 
 
-
-// src/index.ts
 import express from 'express';
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from '@apollo/server/express4';
